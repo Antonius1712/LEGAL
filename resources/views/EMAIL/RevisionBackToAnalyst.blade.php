07d3312226485c4122ccb96b62faa4d61db0881d
Anton <ds01.ho@lippoinsurance.com>
2022-11-03 10:57:29 +0700

    change url on email (get from env app_url

--- resources/views/EMAIL/RevisionBackToAnalyst.blade.php
+++ resources/views/EMAIL/RevisionBackToAnalyst.blade.php
@@ -1,6 +1,6 @@
 <p>Dear Analyst Claim MV</p>
 <p>Pengajuan Review Dokumen Check Sheet direvisi oleh {{ $user_reject }} - {{ $jabatan_reject }}</p>
 <p>Tanggal pengajuan : {{ $tanggal }} pukul {{ $jam }}</p>
-<p>Silahkan diperiksa di web <a href="http://demo-app.lippoinsurance.com:82/">http://demo-app.lippoinsurance.com:82/</a> untuk melihat detailnya.</p>
+<p>Silahkan diperiksa di web <a href="{{ env("APP_URL") }}">{{ env("APP_URL") }}</a> untuk melihat detailnya.</p>
 <p>Mohon untuk tidak membalas email ini.</p>
 <p>Thank You</p>
--- resources/views/EMAIL/SubmitToLegal.blade.php
+++ resources/views/EMAIL/SubmitToLegal.blade.php
@@ -2,6 +2,6 @@
 <p>Dokumen Check Sheet telah disetujui oleh Analyst Claim MV</p>
 <p>Tanggal pengajuan : <strong>{{ $jam }} pukul {{ $tanggal }}</strong></p>
 <p>di ajukan oleh : <strong>{{ $user }} - {{ $jabatan }}</strong></p>
-<p>Silahkan diperiksa di web <a href="http://demo-app.lippoinsurance.com:82/">http://demo-app.lippoinsurance.com:82/</a> untuk melihat detailnya.</p>
+<p>Silahkan diperiksa di web <a href="{{ env("APP_URL") }}">{{ env("APP_URL") }}</a> untuk melihat detailnya.</p>
 <p>Mohon untuk tidak membalas email ini</p>
 <p>Thank You</p>
